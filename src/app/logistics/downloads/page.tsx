32ae376701ba7cd0d21292f4c982d5d3903e7f7f
luissalazarmvd <luis.salazar@vetadorada.pe>
2026-05-12 15:30:56 -0500

    a

--- src/app/logistics/downloads/page.tsx
+++ src/app/logistics/downloads/page.tsx
@@ -2,6 +2,7 @@
 "use client";
 
 import LogisticsMreqStatusTable from "../../../components/logistics/LogisticsReqStatusTable";
+import LogisticsMRATable from "../../../components/logistics/LogisticsMRATable";
 
 const PBI_LOGISTICS_URL =
   "https://app.powerbi.com/view?r=eyJrIjoiYjUzZWY1ZTMtZWQyYi00MmIzLWE1YmMtN2Q2MTFjNzNkMmFmIiwidCI6IjYzNzhiZmNkLWRjYjktNDMwZi05Nzc4LWRiNTk3NGRjMmFkYyIsImMiOjR9";
@@ -19,6 +20,20 @@ export default function LogisticsDownloadsPage() {
         }}
       >
         <LogisticsMreqStatusTable />
+      </div>
+
+      <div style={{ height: 6 }} />
+
+      <div
+        style={{
+          width: "100%",
+          maxWidth: "100%",
+          height: "calc(100vh - 140px)",
+          minHeight: 0,
+          overflow: "hidden",
+        }}
+      >
+        <LogisticsMRATable />
       </div>
 
       <div style={{ height: 6 }} />
